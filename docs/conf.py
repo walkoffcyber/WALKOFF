7a65cf577f2edd684cf699b10c021f205f9742ef
Hannah Burke <burke_hannah@bah.com>
2019-06-03 14:08:32 -0400

    testing open api package

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.ifconfig',
               'sphinx.ext.viewcode',
               'sphinx.ext.napoleon',
-              'sphinxcontrib.httpdomain'
+              'sphinxcontrib.openapi'
               ]
 
 # Add any paths that contain templates here, relative to this directory.
